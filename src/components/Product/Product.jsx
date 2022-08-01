d4f665480eba11547bc80e2b4785cfaeeaacda8d
KadambariH <kadambarihulloli4@gmail.com>
2022-08-01 18:29:02 +0530

    completed app project

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Product.css';
+
+const Product = ({ imgSrc, productTitle, productDescription, productPrice }) => {
+    return (
+        <div className='ProductContainer'>
+            <img src={imgSrc} height='320' weight='320' />
+            <div>
+                <p className='productName' title={productTitle}>{productTitle}</p>
+                <p>{productDescription}</p>
+            </div>
+            <div className='likes'>
+                <p className='productPrice'>Rs: {productPrice}</p>
+                <i class="fa-solid fa-thumbs-up"></i>
+            </div>
+             </div>
+    )
+}
+
+export default Product;
